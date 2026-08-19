52fa397d49a81ee49fa6adde5ba1d94f255c71aa
Liu-Yuqi-617 <e1513087@u.nus.edu>
2026-08-19 11:45:15 +0800

    Rename main class

---
+++ src/main/java/Victoria.java
@@ -0,0 +1,19 @@
+public class Victoria {
+    public static void main(String[] args) {
+        String banner = "██╗   ██╗██╗ ██████╗████████╗ ██████╗ ██████╗ ██╗ █████╗ \n"
+                + "██║   ██║██║██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██║██╔══██╗\n"
+                + "██║   ██║██║██║        ██║   ██║   ██║██████╔╝██║███████║\n"
+                + "╚██╗ ██╔╝██║██║        ██║   ██║   ██║██╔══██╗██║██╔══██║\n"
+                + " ╚████╔╝ ██║╚██████╗   ██║   ╚██████╔╝██║  ██║██║██║  ██║\n"
+                + "  ╚═══╝  ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝\n";
+        String greetings = "Hi! I'm Victoria. Anything new for today?";
+        String h_line = "---------------------------------------------------------------";
+        String farewell = "So nice to chat with you today! See you next time~";
+
+        System.out.println(banner);
+        System.out.println(h_line);
+        System.out.println(greetings);
+        System.out.println(h_line);
+        System.out.println(farewell);
+    }
+}
